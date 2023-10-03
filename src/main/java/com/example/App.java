fb5dfb8bca06e0bd7390e0a14648f34c632e308c
lai2-01 <lai2-01@itismeucci.com>
2023-10-03 09:29:57 +0200

    coppato il codice

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+        System.out.println( "Ti ascolto" );
+        ServerSocket server = new ServerSocket(3000);
+
+        Socket s =server.accept();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        String stringaRicevuta = in.readLine();
+
+        String stringaModificata= stringaRicevuta.toUpperCase();
+        out.writeBytes(stringaModificata + '\n');
+
+        server.close();
+        s.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server del server");
+        }
     }
 }
